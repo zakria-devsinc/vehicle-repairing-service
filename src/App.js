1d0902add174123e690f6a3ae7cf6b7b6eda6f97
zakria-devsinc <muhammad.zakria3637@gmail.com>
2022-05-17 02:38:53 +0500

    un resolved lazy loading

--- src/App.js
+++ src/App.js
@@ -1,82 +1,92 @@
-import './App.css';
-import Home from './shared/home/home';
+import "./App.css";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import Announce from './shared/announcement/announce';
-import Header from './shared/header/header';
-import Footer from './shared/footer/footer';
-import About from './shared/About/About';
-import Electric from './shared/Services/Electric';
-import Engine from './shared/Services/Engine';
-import OIl from './shared/Services/OIl';
-import Tyres from './shared/Services/Tyres';
-import CarRepair from './shared/Hybrids/CarRepair';
-import BatteryRepair from './shared/Hybrids/BatteryRepair';
-import Vehicle from './shared/Vehicle/Vehicle';
-import Coupons from './shared/Coupons/Coupons';
-import Appointment from './shared/Appointment/Appointment';
-import Review from './shared/Review/Review';
-import LeaveAReview from './shared/Review/LeaveAReview';
-import Directions from './shared/Directions/Directions';
-import Contact from './shared/Contact/Contact';
-import LoadingSpinner from './shared/LoadingSpinner/LoadingSpinner';
+import LoadingSpinner from "./shared/LoadingSpinner/LoadingSpinner";
+const Home = lazy(() => {
+  return new Promise(resolve =>  import("./shared/home/home")).then(
+    () =>
+         import("./shared/home/home")
+  );
+});
+// const Home = lazy(() => import("./shared/home/home").default);
+// const Announce = lazy(() => import("./shared/announcement/announce").default);
+// const Header = lazy(() => import("./shared/header/header").default);
+// const Footer = lazy(() => import("./shared/footer/footer").default);
+// const About = lazy(() => import("./shared/About/About").default);
+// const Electric = lazy(() => import("./shared/Services/Electric").default);
+// const Engine = lazy(() => import("./shared/Services/Engine").default);
+// const OIl = lazy(() => import("./shared/Services/OIl").default);
+// const Tyres = lazy(() => import("./shared/Services/Tyres").default);
+// const CarRepair = lazy(() => import("./shared/Hybrids/CarRepair").default);
+// const BatteryRepair = lazy(() => import("./shared/Hybrids/BatteryRepair").default);
+// const Vehicle = lazy(() => import("./shared/Vehicle/Vehicle").default);
+// const Coupons = lazy(() => import("./shared/Coupons/Coupons").default);
+// const Appointment = lazy(() => import("./shared/Appointment/Appointment").default);
+// const Review = lazy(() => import("./shared/Review/Review").default);
+// const LeaveAReview = lazy(() => import("./shared/Review/LeaveAReview").default);
+// const Directions = lazy(() => import("./shared/Directions/Directions").default);
+// const Contact = lazy(() => import("./shared/Contact/Contact").default);
 
 function App() {
   return (
     <BrowserRouter>
-      <Announce /> 
-       <Header /> 
+    {/* <Suspense fallback={<LoadingSpinner/>}>
+      <Announce />
+      <Header />
+      </Suspense> */}
       <Switch>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-        <Route exact path="/about-us">
-          <About />
-        </Route>
-        <Route exact path="/home">
-          {/* <LoadingSpinner/> */}
-          <Home />
-        </Route>
-        <Route exact path="/services">
-          <Electric />
-        </Route>
-        <Route exact path="/services/engine">
-          <Engine />
-        </Route>
-        <Route exact path="/services/oil">
-          <OIl />
-        </Route>
-        <Route exact path="/services/tyres">
-          <Tyres />
-        </Route>
-        <Route exact path="/hybrid">
-          <CarRepair />
-        </Route>
-        <Route exact path="/hybrid/battery">
-          <BatteryRepair />
-        </Route>
-        <Route exact path="/vehicles">
-          <Vehicle />
-        </Route>
-        <Route exact path="/coupons">
-          <Coupons />
-        </Route>
-        <Route exact path="/appointments">
-          <Appointment />
-        </Route>
-        <Route exact path="/reviews">
-          <Review />
-        </Route>
-        <Route exact path="/reviews/leave">
-          <LeaveAReview />
-        </Route>
-        <Route exact path="/directions">
-          <Directions />
-        </Route>
-        <Route exact path="/contactus">
-          <Contact />
-        </Route>
+        <Suspense fallback={<LoadingSpinner/>}>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          {/* <Route exact path="/about-us">
+            <About />
+          </Route>
+          <Route exact path="/services">
+            <Electric />
+          </Route>
+          <Route exact path="/services/engine">
+            <Engine />
+          </Route>
+          <Route exact path="/services/oil">
+            <OIl />
+          </Route>
+          <Route exact path="/services/tyres">
+            <Tyres />
+          </Route>
+          <Route exact path="/hybrid">
+            <CarRepair />
+          </Route>
+          <Route exact path="/hybrid/battery">
+            <BatteryRepair />
+          </Route>
+          <Route exact path="/vehicles">
+            <Vehicle />
+          </Route>
+          <Route exact path="/coupons">
+            <Coupons />
+          </Route>
+          <Route exact path="/appointments">
+            <Appointment />
+          </Route>
+          <Route exact path="/reviews">
+            <Review />
+          </Route>
+          <Route exact path="/reviews/leave">
+            <LeaveAReview />
+          </Route>
+          <Route exact path="/directions">
+            <Directions />
+          </Route>
+          <Route exact path="/contactus">
+            <Contact />
+          </Route> */}
+        </Suspense>
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
